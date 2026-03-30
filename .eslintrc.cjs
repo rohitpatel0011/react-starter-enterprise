8e1353f3516398ddab44b017e418e9a7df8cbe53
M Sohaib <www.msohaib422@gmail.com>
2026-03-30 22:58:02 +0500

    fomatting done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react", "@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
   settings: {
-    react: { version: "detect" }
+    react: { version: 'detect' },
   },
   rules: {
-    "react/react-in-jsx-scope": "off"
-  }
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- .github/ISSUE_TEMPLATE/feature_request.md
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -1,17 +1,20 @@
 ---
 name: ✨ Feature Request
 about: Suggest a new feature
-title: "[FEATURE]: "
+title: '[FEATURE]: '
 labels: enhancement
 ---
 
 ## ✨ Feature Description
+
 Describe the feature clearly.
 
 ## 🎯 Use Case
+
 Why is this feature useful?
 
 ## 🔄 Alternatives
+
 Any alternative solutions considered?
 
 ## 📌 Additional Context
